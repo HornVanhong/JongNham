e122e64342bfeade825fce8b5612ec4e240627e0
ChanLiitaa <149246779+ChanLiitaa@users.noreply.github.com>
2024-10-30 05:07:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        });
+}
